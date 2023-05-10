954df99e9062a3986306e187d92599b603b3c643
Seyf_CHERNI <73757234+SeyfCHERNI@users.noreply.github.com>
2023-05-10 19:46:21 +0200

    fix bug annotations

--- src/test/java/com/example/demo/repositories/StudentRepositoryTest.java
+++ src/test/java/com/example/demo/repositories/StudentRepositoryTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.data.domain.Page;
+
 import org.springframework.data.domain.PageRequest;
 
 import com.example.demo.entities.ClasseSchool;
